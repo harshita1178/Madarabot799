41e5b9701bbd8f5b208a28ed0c67c1cf4fb90204
harshita1178 <usekarlomujhe@gmail.com>
2025-03-25 00:27:23 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,15 +5,15 @@ class Config(object):
     OWNER_ID = "7885908019"
     sudo_users = "7885908019", "7640076990", "7756901810" 
     GROUP_ID =-1002640379822
-    TOKEN = "7583740884:AAEsWTCQNJiOjNQdXw4MBFU9-rL5l82mLQA" 
+    TOKEN = "7673948265:AAFfj9piGxd0e-0bdV-eePwnjd5hIRUyFXg" 
     mongo_url = "mongodb+srv://naruhina:harshitkichut@cluster0.03pl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
     PHOTO_URL = "https://files.catbox.moe/0nb1p7.jpg" 
-    SUPPORT_CHAT = "blade_x_support"
-    UPDATE_CHAT = "blade_x_community"
-    BOT_USERNAME = "Devine_wifu_bot"
-    CHARA_CHANNEL_ID = "-1002640379822"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "https://t.me/Anime_Circle_Club"
+    UPDATE_CHAT = "https://t.me/+D_z2g--T1UJjOWM1"
+    BOT_USERNAME = "Madara_Husbando_grabber_Bot"
+    CHARA_CHANNEL_ID = "-1002552013602"
+    api_id = 28159105
+    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
 
     
 class Production(Config):
